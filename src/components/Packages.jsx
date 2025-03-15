4eeda4a38a7ca953b06b0987a8ea0efda5177810
M. Diki Iswari <141617567+dikymonz@users.noreply.github.com>
2025-03-15 13:04:44 +0800

    Update Packages.jsx

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -96,33 +96,107 @@ const Packages = () => {
         ))}
       </div>
 
-      {/* Modal Detail */}
-      {selectedPackage && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-          <section
-            className="bg-white dark:bg-gray-900 text-center p-6 py-20 lg:px-32 w-full overflow-y-auto max-h-screen"
-            id="detail"
-          >
-            <button
-              onClick={() => setSelectedPackage(null)}
-              className="absolute top-4 right-4 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold rounded-full w-8 h-8 flex items-center justify-center"
-            >
-              ✕
-            </button>
-            <div className="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
-              <div className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                <h2 className="text-left mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-                  {selectedPackage.title}
-                </h2>
-                <p className="text-left">{selectedPackage.desk} - {selectedPackage.route}</p>
-                <p className="text-left">{selectedPackage.desk1}</p>
-              </div>
-              <div className="grid grid-cols-2 gap-4 mt-8">
-                <img className="w-full rounded-lg" src={selectedPackage.image1} alt="content1" />
-                <img className="mt-4 w-full lg:mt-10 rounded-lg" src={selectedPackage.image2} alt="content2" />
-              </div>
+       {/* Modal */}
+       {selectedPackage && (
+        <div className='fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50'>
+            {/* Tombol Close */}
+            <section className='bg-white dark:bg-gray-900 text-center p-6 py-20 lg:px-32 w-full overflow-y-auto max-h-screen' id='detail'>
+                
+                        <div className='gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6'>
+                        <button
+        onClick={() => setSelectedPackage(null)}
+        className='absolute top-4 right-4 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold rounded-full w-8 h-8 flex items-center justify-center'
+      >
+        ✕
+      </button>
+                            <div className='font-light text-gray-500 sm:text-lg dark:text-gray-400'>
+                                <h2 className='text-left mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white'>{selectedPackage.title}</h2>
+                                <p className='text-left'>{selectedPackage.desk} - {selectedPackage.route}</p>
+                                <p className='text-left'>{selectedPackage.desk1}</p>
+                                <div className='mt-2'>
+                                <h2 class="text-left mb-2 text-lg font-semibold text-gray-900 dark:text-white">Includes:</h2>
+                <ul className="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                <li className="flex items-center">
+                    {selectedPackage.include1}
+                </li>
+                <li className="flex items-center">
+                     {selectedPackage.include2}
+                </li>
+                <li className="flex items-center ">
+                     {selectedPackage.include3}
+                </li>
+                <li className="flex items-center">
+                     {selectedPackage.include4}
+                </li>
+                <li className="flex items-center">
+                     {selectedPackage.include5}
+                </li>
+                <li className="flex items-center">
+                     {selectedPackage.include6}
+                </li>
+                <li className="flex items-center">
+                     {selectedPackage.include7}
+                </li>
+                <li className="flex items-center">
+                     {selectedPackage.include8}
+                </li>
+            </ul>
+            
+            {/* table prices */}
+            
+            <div className='Tabel mt-5'>
+            <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                package price list
+                        <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">For orders of more than 6 people, please contact us directly on WhatsApp.</p>
+                    </caption>
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3">
+                                1 Person
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                2-3 Person
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                4-5 Person
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                6-7 Person
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                            <td scope="row" class="px-6 py-4">
+                                {selectedPackage.price1}
+                            </td>
+                            <td class="px-6 py-4">
+                                {selectedPackage.price2}
+                            </td>
+                            <td class="px-6 py-4">
+                                {selectedPackage.price3}
+                            </td>
+                            <td class="px-6 py-4">
+                                {selectedPackage.price4}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-          </section>
+            <div className='text-left'>
+            <button type="button" className="mt-5 text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Booking Now</button>
+            </div>
+            </div>
+                            </div>
+                            </div>
+                            <div className='grid grid-cols-2 gap-4 mt-8'>
+                                <img className='w-full rounded-lg' src={selectedPackage.image1} alt="content1" />
+                                <img className='mt-4 w-full lg:mt-10 rounded-lg' src={selectedPackage.image2} alt="content2" />
+                            </div>
+                        </div>
+                    </section>
         </div>
       )}
     </motion.div>
